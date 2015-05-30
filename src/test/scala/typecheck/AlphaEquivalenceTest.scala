e0f2da8619f59cb07d12a8545cfe6fdc2f05529b
Dima Gerasimov <karlicoss@gmail.com>
2015-05-30 14:32:17 +0300

    more tests for alpha equivalence

--- src/test/scala/typecheck/AlphaEquivalenceTest.scala
+++ src/test/scala/typecheck/AlphaEquivalenceTest.scala
@@ -9,15 +9,23 @@ import util.UnitSpec
 /**
  * Created by karlicos on 30.05.15.
  */
-class AlphaTest extends UnitSpec {
+class AlphaEquivalenceTest extends UnitSpec {
   def makeId(name: String): Lam = {
     val v = vv(name)
     Lam(Abs(v, Universe(0), Var(v)))
   }
 
-  "fdsf" should "fsdfs" in {
+  it should "treat as equivalent" in {
     val one = makeId("x")
     val two = makeId("y")
     equivalent(one, two) should be (true)
+  }
+
+  it should "treat universes with same indices as eqivalent" in {
+    equivalent(Universe(11), Universe(11)) should be (true)
+  }
+
+  it should "treat universes with different indices as not equivalent" in {
+    equivalent(Universe(1), Universe(2)) should be (false)
   }
 }
