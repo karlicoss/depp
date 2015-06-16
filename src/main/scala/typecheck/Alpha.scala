06b9797e7048dc9e0be69d3aa967f4399f778992
Dima Gerasimov <karlicoss@gmail.com>
2015-06-16 22:43:11 +0300

    renamed misleading variable

--- src/main/scala/typecheck/Alpha.scala
+++ src/main/scala/typecheck/Alpha.scala
@@ -9,33 +9,33 @@ import terms._
  */
 object Alpha {
   def equivalent(a: Term, b: Term): Boolean = {
-    def absHelper(ctx: Map[Variable, Variable], a: Abs, b: Abs): Boolean = {
-      val typeEquiv = helper(ctx, a.tp, b.tp)
-      val bodyEquiv = helper(ctx + (a.v -> b.v), a.body, b.body)
+    def absHelper(map: Map[Variable, Variable], a: Abs, b: Abs): Boolean = {
+      val typeEquiv = helper(map, a.tp, b.tp)
+      val bodyEquiv = helper(map + (a.v -> b.v), a.body, b.body)
       typeEquiv && bodyEquiv
     }
 
-    def helper(ctx: Map[Variable, Variable], a: Term, b: Term): Boolean = {
+    def helper(map: Map[Variable, Variable], a: Term, b: Term): Boolean = {
       a match {
         case Var(aname) =>
           b match {
-            case Var(bname) => ctx(aname) == bname
+            case Var(bname) => map(aname) == bname
             case _ => false
           }
         case Lam(aabs) =>
           b match {
-            case Lam(babs) => absHelper(ctx, aabs, babs)
+            case Lam(babs) => absHelper(map, aabs, babs)
             case _ => false
           }
         case App(a1, a2) =>
           b match {
             case App(b1, b2) =>
-              helper(ctx, a1, b1) && helper(ctx, a2, b2)
+              helper(map, a1, b1) && helper(map, a2, b2)
             case _ => false
           }
         case Pi(aabs) =>
           b match {
-            case Pi(babs) => absHelper(ctx, aabs, babs)
+            case Pi(babs) => absHelper(map, aabs, babs)
             case _ => false
           }
         case Level(akind) =>
